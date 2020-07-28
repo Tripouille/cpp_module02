08b225563f7cd3f7967f6370a650be723bfaff20
jgambard <jgambard@student.42lyon.fr>
2020-07-28 19:29:47 +0200

    add test for ex03 + result of bc

--- ex03/test.sh
+++ ex03/test.sh
@@ -3,10 +3,15 @@ make
 function test
 {
 	printf "./eval_expr $1\n"
-	echo -n "RESULT = " && ./eval_expr "$1"
+	echo -n "my result = " && ./eval_expr "$1"
+	echo -n "bc result = "
+	echo "$1" | bc
 }
 test "(18.18 + 3.03) * 2"
 test "(1) + (8 * 2 + 5 - 2 * 10)"
 test "(-1 - 10 * ((2 + 5) * 5) + 1)"
 test "(-1 - -2) * 5"
+test "(-1 - (((-2 + 4 * (5 + 1)) * 2) * 10)* 10) * 10 + 0"
+test "(2 * 8 / 9 + 2 - -5) / 5 + 5 * (25 * 2) - 12 * 1"
+test "0 + 0"
 make fclean
